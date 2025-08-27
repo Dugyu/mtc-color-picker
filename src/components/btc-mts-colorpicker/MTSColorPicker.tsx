9610fc8cf336172d5fe6d549487525c9ff31f182
Dugyu <23237626+Dugyu@users.noreply.github.com>
2025-08-27 21:22:02 +0800

    feat(mts-colorpicker): implement MTSColorPicker, demo setup

---
+++ src/components/btc-mts-colorpicker/MTSColorPicker.tsx
@@ -0,0 +1,91 @@
+import { useCallback, useMainThreadRef } from '@lynx-js/react';
+import {
+  HueSlider,
+  LightnessSlider,
+  SaturationSlider,
+} from '@/components/btc-mts-slider/MTSSlider';
+import type { MTSWriter } from '@/components/btc-mts-slider/MTSSlider';
+
+interface ColorPickerProps {
+  initialHSL?: readonly [number, number, number];
+  onMTSHSLChange?: (next: readonly [h: number, s: number, l: number]) => void;
+}
+function ColorPicker({
+  initialHSL = [199, 99, 72],
+  onMTSHSLChange,
+}: ColorPickerProps) {
+  const mtsHueRef = useMainThreadRef<number>(initialHSL[0]);
+  const mtsWriteSL = useMainThreadRef<MTSWriter<readonly [number, number]>>();
+
+  const mtsSaturationRef = useMainThreadRef<number>(initialHSL[1]);
+  const mtsWriteHL = useMainThreadRef<MTSWriter<readonly [number, number]>>();
+
+  const mtsLightnessRef = useMainThreadRef<number>(initialHSL[2]);
+  const mtsWriteHS = useMainThreadRef<MTSWriter<readonly [number, number]>>();
+
+  const writeSliderGradients = useCallback(() => {
+    'main thread';
+    mtsWriteSL?.current?.([mtsSaturationRef.current, mtsLightnessRef.current]);
+    mtsWriteHL?.current?.([mtsHueRef.current, mtsLightnessRef.current]);
+    mtsWriteHS?.current?.([mtsHueRef.current, mtsSaturationRef.current]);
+  }, []);
+
+  const forwardOnHSLChange = useCallback(() => {
+    'main thread';
+    writeSliderGradients();
+    onMTSHSLChange?.([
+      mtsHueRef.current,
+      mtsSaturationRef.current,
+      mtsLightnessRef.current,
+    ]);
+  }, [onMTSHSLChange]);
+
+  const onMTSHueChange = useCallback(
+    (h: number) => {
+      'main thread';
+      mtsHueRef.current = h;
+      forwardOnHSLChange();
+    },
+    [forwardOnHSLChange],
+  );
+
+  const onMTSSaturationChange = useCallback(
+    (s: number) => {
+      'main thread';
+      mtsSaturationRef.current = s;
+      forwardOnHSLChange();
+    },
+    [forwardOnHSLChange],
+  );
+
+  const onMTSLightnessChange = useCallback(
+    (l: number) => {
+      ('main thread');
+      mtsLightnessRef.current = l;
+      forwardOnHSLChange();
+    },
+    [forwardOnHSLChange],
+  );
+
+  return (
+    <view className="w-full h-full flex flex-col gap-y-4">
+      <HueSlider
+        onMTSChange={onMTSHueChange}
+        initialSL={[initialHSL[1], initialHSL[2]]}
+        mtsWriteSL={mtsWriteSL}
+      />
+      <SaturationSlider
+        onMTSChange={onMTSSaturationChange}
+        initialHL={[initialHSL[0], initialHSL[2]]}
+        mtsWriteHL={mtsWriteHL}
+      />
+      <LightnessSlider
+        onMTSChange={onMTSLightnessChange}
+        initialHS={[initialHSL[0], initialHSL[1]]}
+        mtsWriteHS={mtsWriteHS}
+      />
+    </view>
+  );
+}
+
+export { ColorPicker };
---
+++ src/demos/BTCMTSColorPicker.tsx
@@ -0,0 +1,36 @@
+import { root, runOnBackground, useCallback, useState } from '@lynx-js/react';
+import { AppLayout } from '@/App';
+
+import { ColorPicker } from '@/components/btc-mts-colorpicker/MTSColorPicker';
+
+export function App() {
+  const [value, setValue] = useState<readonly [number, number, number]>([
+    199, 99, 72,
+  ]);
+
+  const onMTSHSLChange = useCallback(
+    (next: readonly [number, number, number]) => {
+      'main thread';
+      runOnBackground(setValue)(next);
+    },
+    [],
+  );
+
+  return (
+    <AppLayout
+      title="BTC-MTS ColorPicker"
+      h={value[0]}
+      s={value[1]}
+      l={value[2]}
+    >
+      <view className="w-60 h-12 flex-row justify-center items-center">
+        <text className="text-content">{`${value}`}</text>
+      </view>
+      <view className="w-60 h-48">
+        <ColorPicker initialHSL={value} onMTSHSLChange={onMTSHSLChange} />
+      </view>
+    </AppLayout>
+  );
+}
+
+root.render(<App />);
